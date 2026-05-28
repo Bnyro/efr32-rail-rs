de4b832d3b285b08a318240177d3d301ed9fe28a
Bnyro <bnyro@tutanota.com>
2026-05-28 16:38:15 +0200

    chore: fix cc compiler warnings about redefined symbols

--- c-inc/definitions.h
+++ c-inc/definitions.h
@@ -1,2 +1,16 @@
+// set the specific model of the chip
+#ifndef EFR32MG22E224F512IM40
 #define EFR32MG22E224F512IM40 1
+#endif
+
+// used for trustzone startup logic
+// idk why this is needed as we don't use Trustzone?
+// but it's required for compilation
+#ifndef __ARM_FEATURE_CMSE_OVERRIDEN
+
+#undef __ARM_FEATURE_CMSE
 #define __ARM_FEATURE_CMSE 3U
+
+#define __ARM_FEATURE_CMSE_OVERRIDEN 1
+
+#endif
